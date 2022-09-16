d2e48f4bb9474d1945d222f6efdd80de17a29c88
xXWeIsLyXx <weihang.zheng@mail.utoronto.ca>
2022-09-15 20:51:22 -0400

    resume published from docs

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,52 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 import Main from '../layouts/Main';
-
-import Education from '../components/Resume/Education';
-import Experience from '../components/Resume/Experience';
-import Skills from '../components/Resume/Skills';
-import Courses from '../components/Resume/Courses';
-import References from '../components/Resume/References';
-
-import courses from '../data/resume/courses';
-import degrees from '../data/resume/degrees';
-import positions from '../data/resume/positions';
-import { skills, categories } from '../data/resume/skills';
-
-const sections = [
-  'Education',
-  'Experience',
-  'Skills',
-  'Courses',
-  'References',
-];
 
 const Resume = () => (
   <Main
     title="Resume"
     description="Michael D'Angelo's Resume. Arthena, Matroid, YC, Skeptical Investments, Stanford ICME, Planet Labs, and Facebook."
   >
-    <article className="post" id="resume">
-      <header>
-        <div className="title">
-          <h2 data-testid="heading"><Link to="resume">Resume</Link></h2>
-          <div className="link-container">
-            {sections.map((sec) => (
-              <h4 key={sec}>
-                <a href={`#${sec.toLowerCase()}`}>{sec}</a>
-              </h4>))}
-          </div>
-
-        </div>
-      </header>
-      <Education data={degrees} />
-      <Experience data={positions} />
-      <Skills skills={skills} categories={categories} />
-      <Courses data={courses} />
-      <References />
-
-    </article>
+    <iframe title="myFrame" src="https://docs.google.com/document/d/e/2PACX-1vTsIuUIf9PpB5ZcLYpAi6JW_xDFHJWduH-e-oEOOTuJ4khjmiru_RPecj1xfJ8ib3uami_zp-iEb6Vw/pub?embedded=true" width="800" height="1500" />
   </Main>
 );
 
