454f0a0529bde7f664b39d7f3b9e8acd3e6d8fae
xXWeIsLyXx <weihang.zheng@mail.utoronto.ca>
2022-09-18 13:09:19 -0400

    stat

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -37,7 +37,7 @@ const data = [
   },
   {
     key: 'personality',
-    label: 'Personality Type',
+    label: 'MBTI Personality Type',
     value: 'INTJ-A (94I, 66N, 100T, 100J)',
     link: 'https://www.16personalities.com/intj-personality',
   },
