dea0f04a3418997e1c20e25a6d3871c5d929c4d1
xXWeIsLyXx <weihang.zheng@mail.utoronto.ca>
2022-09-18 00:02:30 -0400

    fix

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -20,10 +20,8 @@ const Index = () => (
       </header>
       <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
         or you can check out my {' '}
-        <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
+        <Link to="/resume">resume</Link> and {' '}
+        <Link to="/projects">projects</Link>. {' '}
       </p>
       <p> Source available <a href="https://github.com/weihangzheng/personal-site">here</a>.</p>
     </article>
