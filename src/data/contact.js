a304385f5e00d0a900b3244dcd523538c3cd3888
xXWeIsLyXx <weihang.zheng@mail.utoronto.ca>
2022-09-18 12:32:16 -0400

    stats

--- src/data/contact.js
+++ src/data/contact.js
@@ -2,6 +2,8 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube';
+import { faTiktok } from '@fortawesome/free-brands-svg-icons/faTiktok';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
@@ -26,6 +28,16 @@ const data = [
     link: 'https://www.linkedin.com/in/weihang-zheng/',
     label: 'LinkedIn',
     icon: faLinkedinIn,
+  },
+  {
+    link: 'https://www.youtube.com/channel/UCsYCpANRJkWQoan-uQ7c4FQ',
+    label: 'YouTube',
+    icon: faYoutube,
+  },
+  {
+    link: 'https://www.tiktok.com/@melandwei',
+    label: 'TikTok',
+    icon: faTiktok,
   },
   {
     link: 'mailto:weihang.zheng@mail.utoronto.ca',
