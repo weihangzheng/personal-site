cf7c6e3102ae6a94e83cc2fb305f0e4ee4131983
xXWeIsLyXx <weihang.zheng@mail.utoronto.ca>
2022-10-02 00:07:26 -0400

    new

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,10 +1,7 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
-import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube';
-import { faTiktok } from '@fortawesome/free-brands-svg-icons/faTiktok';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 
@@ -15,29 +12,9 @@ const data = [
     icon: faGithub,
   },
   {
-    link: 'https://facebook.com/xXWeIsLyXx/',
-    label: 'Facebook',
-    icon: faFacebookF,
-  },
-  {
-    link: 'https://www.instagram.com/weihang9580/',
-    label: 'Instagram',
-    icon: faInstagram,
-  },
-  {
     link: 'https://www.linkedin.com/in/weihang-zheng/',
     label: 'LinkedIn',
     icon: faLinkedinIn,
-  },
-  {
-    link: 'https://www.youtube.com/channel/UCsYCpANRJkWQoan-uQ7c4FQ',
-    label: 'YouTube',
-    icon: faYoutube,
-  },
-  {
-    link: 'https://www.tiktok.com/@melandwei',
-    label: 'TikTok',
-    icon: faTiktok,
   },
   {
     link: 'mailto:weihang.zheng@mail.utoronto.ca',
